b86be9c5cbb53ff6add36cdf6815dd4ef7310e6d
David M. Smiley <davidmsmiley@gmail.com>
2021-03-01 14:51:33 -0500

    Update Regression_London-Housing.py

--- Regression_London-Housing.py
+++ Regression_London-Housing.py
@@ -6,56 +6,46 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
 
 st.write("""
-# Boston House Price Prediction App
-This app predicts the **Boston House Price**!
+# London House Price Prediction App
+Use the sidebar to set your parameters for:
+- Square footage
+- Num. of Bedrooms
+- Num. of Bathrooms
+- Num. of Receptions
 """)
 st.write('---')
 
 # Loads the Boston House Price Dataset
-column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-boston = pd.read_csv('/data/housing.csv', header=None, delimiter=r"\s+", names=column_names)
-column_sels = ['LSTAT', 'INDUS', 'NOX', 'PTRATIO', 'RM', 'TAX', 'DIS', 'AGE']
-X = boston.loc[:,column_sels]
-Y = boston['MEDV']
+london = pd.read_csv('/Users/David/PycharmProjects/Analytics_with_Streamlit/data/London.csv', index_col=0)
+# Drops non-numeric columns
+london = london.drop(columns=['Property Name', 'House Type', 'Location', 'City/County', 'Postal Code'])
+# Renames existing columns
+london = london.rename(columns={'Price':'PRICE', 'Area in sq ft': 'SQFT', 'No. of Bedrooms': 'BEDS', 'No. of Bathrooms' : 'BATHS', 'No. of Receptions': "RECS"})
+# Turns all columns in df to float dtype
+london = london.astype(float)
+
+# Preparing training and testing datasets
+X = london.drop(columns=['PRICE'])
+Y = london['PRICE']
 
 # Sidebar
 # Header of Specify Input Parameters
 st.sidebar.header('Specify Input Parameters')
-
 def user_input_features():
-    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-    B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'B': B,
-            'LSTAT': LSTAT}
+    SQFT = st.sidebar.slider('SQFT', X.SQFT.min(), X.SQFT.max(), X.SQFT.mean())
+    BEDS = st.sidebar.slider('BEDS', X.BEDS.min(), X.BEDS.max(), X.BEDS.mean())
+    BATHS = st.sidebar.slider('BATHS', X.BATHS.min(), X.BATHS.max(), X.BATHS.mean())
+    RECS = st.sidebar.slider('RECS', X.RECS.min(), X.RECS.max(), X.RECS.mean())
+    data = {'SQFT': SQFT,
+            'BEDS': BEDS,
+            'BATHS': BATHS,
+            'RECS': RECS}
     features = pd.DataFrame(data, index=[0])
     return features
 
 df = user_input_features()
 
 # Main Panel
-
 # Print specified input parameters
 st.header('Specified Input parameters')
 st.write(df)
@@ -64,12 +54,16 @@ st.write('---')
 # Build Regression Model
 model = RandomForestRegressor()
 model.fit(X, Y)
+
 # Apply Model to Make Prediction
 prediction = model.predict(df)
 
-st.header('Prediction of MEDV')
-st.write(prediction)
+#Reformat predication score from np.array to $ amount
+st.header('Prediction of PRICE')
+prediction_float = "£{:,.2f}".format(prediction.item(0))
+st.write(prediction_float)
 st.write('---')
+
 
 # Explaining the model's predictions using SHAP values
 # https://github.com/slundberg/shap
@@ -79,6 +73,7 @@ shap_values = explainer.shap_values(X)
 st.header('Feature Importance')
 plt.title('Feature importance based on SHAP values')
 shap.summary_plot(shap_values, X)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot(bbox_inches='tight')
 st.write('---')
 
