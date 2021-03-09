887cbea77c1a5ca44bd4d8f2fcd3059f41f3c299
David M. Smiley <davidmsmiley@gmail.com>
2021-03-09 05:59:17 -0500

    Update Regression_London-Housing.py

--- Regression_London-Housing.py
+++ Regression_London-Housing.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import shap
 import matplotlib.pyplot as plt
-from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
 
 st.write("""
